75992024ab928744d991b4542938928c69cce3ef
David Eugène <edspeedy@gmail.com>
2018-10-04 11:04:01 +0200

    Réagencement : déplacement des fichiers Contact.js, Contacts.js + creation de AddContact.js dans le nouveau dossier contacts - déplacment de Header.js dans nouveau dossier layout - emploi du reducer et dispatch dans context.js

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Contact from "./Contact";
+import { Consumer } from "../../context";
+
+class Contacts extends React.Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { contacts } = value; // Destructuring
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+              {/* On passe l'objet contact directement au lieu de passer chaque propriété séparément */}
+              {/* <Contact key={contact.id} name={contact.name} email={contact.email} phone={contact.phone} /> */}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Contacts;
